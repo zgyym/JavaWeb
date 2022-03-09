0f33b49bc1607b01abd44ec32a6a6efde32521a5
zgyym <2064635072@qq.com>
2022-03-09 22:51:31 +0800

    'thymeleaf'

---
+++ 5--thymeleaf/src/servlet/IndexServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import fruit.dao.FruitDAO;
+import fruit.dao.impl.FruitDAOImpl;
+import fruit.pojo.Fruit;
+import myspringmvc.ViewBaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        List<Fruit> fruitList = fruitDAO.getFruitList();
+        //保存到session作用域
+        HttpSession session = req.getSession();
+        session.setAttribute("fruitList",fruitList);
+
+    }
+}
